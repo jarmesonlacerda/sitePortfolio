43c6c0c866b6f5e55877d4dec279dd129b42a925
jarmesonlacerda <https://github.com/jarmesonlacerda/sitePortfolio.git>
2023-06-16 15:11:27 -0300

    3° página - class services -Navegação secundaria

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-/*função que ativa o elemento*/
-function ativaLetra(elemento) {
-    const arrTexto = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
+function escrevendoLetra() {
+  /*função que ativa o elemento*/
+  function ativaLetra(elemento) {
+    const arrTexto = elemento.innerHTML.split("");
+    elemento.innerHTML = "";
     arrTexto.forEach((letra, i) => {
-        setTimeout(() => {
-            elemento.innerHTML += letra;
-        }, 75 * i);
+      setTimeout(() => {
+        elemento.innerHTML += letra;
+      }, 75 * i);
     });
+  }
+
+  const titulo = document.querySelector(".digitando");
+  ativaLetra(titulo);
 }
 
-const titulo = document.querySelector('.digitando');
-ativaLetra (titulo);
+const ativaMenu = document.querySelector(".bi-list");
 
-
-
-
-
+ativaMenu.addEventListener("click", () => {
+  ativaMenu.classList.toggle("bi-x-lg");
+});
